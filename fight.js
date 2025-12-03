8121ca2b596e2741b790d4fe67d3ca32fc2b72a3
Joshua Ronan <joshronan25@gmail.com>
2025-12-03 22:33:01 +0000

    Second Commit

--- fight.js
+++ fight.js
@@ -10,33 +10,75 @@ canvas.height = 576
 // Background
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+// Gravity -------->
+const gravity = 0.2
+
+
 // Knights -------->
 
 class Sprite {
-  constructor(position){
+  constructor({position, velocity}){
     this.position = position
+    this.velocity = velocity
+    this.height = 150
   }
 
   draw() {
     c.fillStyle = 'red'
-    c.fillRect(this.position.x, this.position.y, 50, 150)
+    c.fillRect(this.position.x, this.position.y, 50, this.height)
+  }
+
+  update() {
+    this.draw()
+    this.position.y += this.velocity.y
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0
+    } else
+      this.velocity.y += gravity
+
   }
 }
 
 // Player
 const player = new Sprite({
+  position:{
   x:0,
   y:0
+},
+velocity: {
+  x: 0,
+  y: 0
+}
 })
 
-player.draw()
 
 // Enemy
 const enemy = new Sprite({
-  x:400,
-  y:100
+  position:{
+  x: 400,
+  y: 100
+},
+velocity: {
+  x: 0,
+  y: 0
+}
 })
 
-enemy.draw()
 
 console.log(player)
+
+function animate() {
+  window.requestAnimationFrame (animate)
+  c.fillStyle = 'black'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  player.update()
+  enemy.update()
+}
+
+animate()
+
+window.addEventListener('keydown' , (event) => {
+  console.log(event.key)
+
+})
