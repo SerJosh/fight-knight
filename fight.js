a42a748fe7547c2a53055d58c48b1ea0d233bd8c
Joshua Ronan <joshronan25@gmail.com>
2025-12-23 20:02:10 +0000

    finish timer,end game and structure of game

--- fight.js
+++ fight.js
@@ -144,17 +144,32 @@ function rectangularCollision({
 }
 
 // Timer
-let timer = 10
+
+function determineWinner({player, enemy, timerId}) {
+  clearTimeout(timerId)
+  document.querySelector('#displayText').style.display = 'flex'
+  if (player.health === enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Tie' 
+  } else if (player.health > enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+}   else if (enemy.health > player.health) {
+    document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+} 
+
+}
+let timer = 60
+let timerId
 function decreaseTimer() {
-  setTimeout (decreaseTimer, 1000)
   if (timer > 0) {
+    timerId = setTimeout (decreaseTimer, 1000)
     timer--
     document.querySelector('#timer').innerHTML = timer
   }
+
+  if (timer === 0) {
+    determineWinner({player, enemy, timerId})
   
-  if (player.health === enemy.health) {
-    console.log('tie')
-  }
+}
 }
 decreaseTimer()
 
@@ -212,6 +227,11 @@ function animate() {
     // Damage
     player.health -= 20
     document.querySelector('#playerHealth').style.width = player.health + '%'
+  }
+
+  // End Game based on health
+  if (enemy.health <= 0 || player.health <= 0) {
+      determineWinner({player, enemy, timerId})
   }
 }
 
